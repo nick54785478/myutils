0e88f8282244cf9ee0a62f2dca555d9e09a55dac
HONEY\nick5 <genghao2021@gmail.com>
2025-07-30 01:48:07 +0800

    調整 BIRT Util

--- src/test/java/com/example/demo/util/BirtReportUtilTest.java
+++ src/test/java/com/example/demo/util/BirtReportUtilTest.java
@@ -5,10 +5,12 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -35,24 +37,13 @@ class BirtReportUtilTest {
 		InputStream inputStream = BirtReportUtil.getResourceInputStream("birt/report", "User.rptdesign");
 		Map<String, Object> params = new HashMap<>();
 		params.put("teacher", "孔老二");
+		params.put("referenceNo", "ref20250728");
 
-		Map<String, Object> context1 = new HashMap<>();
-		context1.put("number", 1);
-		context1.put("name", "王小明");
-		context1.put("score", 100);
-		context1.put("testTime", new Date());
-
-		Map<String, Object> context2 = new HashMap<>();
-		context2.put("number", 2);
-		context2.put("name", "李大牛");
-		context2.put("score", 100);
-		context2.put("testTime", new Date());
-
-		List<Map<String, Object>> dataList = List.of(context1, context2);
+		List<Map<String, Object>> dataList = getMockDataList();
 		Map<String, List<Map<String, Object>>> dataContext = Map.of("dataList", dataList);
 
 		ByteArrayResource resource = BirtReportUtil.generatePdfReport(inputStream, params, dataContext);
-		
+
 		assertNotNull(resource);
 		try {
 			this.downloadLocally(resource);
@@ -70,6 +61,29 @@ class BirtReportUtilTest {
 		try (FileOutputStream fos = new FileOutputStream(outputPath + "/student.pdf")) {
 			fos.write(resource.getContentAsByteArray());
 		}
+	}
+
+	/**
+	 * 生成 DataList 模擬數據
+	 */
+	private static List<Map<String, Object>> getMockDataList() {
+		List<Map<String, Object>> dataList = new ArrayList<>();
+		for (int i = 1; i <= 45; i++) {
+			Map<String, Object> map = new HashMap<>();
+			int chineseScore = (int) (Math.random() * 100); // 隨機取數
+			int mathScore = (int) (Math.random() * 100); // 隨機取數
+			int englishScore = (int) (Math.random() * 100); // 隨機取數
+
+			map.put("number", i);
+			map.put("name", UUID.randomUUID().toString().substring(0, 6)); // 截前六碼
+
+			map.put("chineseScore", chineseScore);
+			map.put("mathScore", mathScore);
+			map.put("englishScore", englishScore);
+			map.put("testTime", new Date());
+			dataList.add(map);
+		}
+		return dataList;
 	}
 
 }
