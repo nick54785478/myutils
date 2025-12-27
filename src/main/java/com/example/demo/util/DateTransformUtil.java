9e1b41fc28a52716d4839f6f7af9d93485ff3b27
HONEY\nick5 <genghao2021@gmail.com>
2025-12-28 03:22:33 +0800

    調整 DateTransformUtil.

--- src/main/java/com/example/demo/util/DateTransformUtil.java
+++ src/main/java/com/example/demo/util/DateTransformUtil.java
@@ -19,9 +19,13 @@ import lombok.NoArgsConstructor;
  */
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class DateTransformUtil {
-	
+
 	/**
 	 * 將 LocalDateTime 轉換為 String
+	 * 
+	 * @param pattern       日期格式
+	 * @param localDateTime 日期資料
+	 * @return 日期字串
 	 */
 	public static String format(String pattern, LocalDateTime localDateTime) {
 		if (localDateTime == null) {
@@ -34,22 +38,42 @@ public class DateTransformUtil {
 
 	/**
 	 * 將 LocalDate 轉為 String
+	 * 
+	 * @param pattern   日期格式
+	 * @param localDate 日期資料
+	 * @return 日期字串
 	 */
-	public static String transformLocalDateToString(LocalDate localDate) {
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+	public static String format(String pattern, LocalDate localDate) {
+		if (localDate == null) {
+			return null;
+		}
+
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
 		return localDate.format(formatter);
 	}
 
 	/**
 	 * 將 String 轉為 LocalDate
+	 * 
+	 * @param pattern   日期格式
+	 * @param localDate 日期資料字串
+	 * @return LocalDate 日期資料
 	 */
-	public static LocalDate transformStringToLocalDate(String localDate) {
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+	public static LocalDate transformStringToLocalDate(String pattern, String localDate) {
+		if (localDate == null) {
+			return null;
+		}
+
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
 		return LocalDate.parse(localDate, formatter);
 	}
 
 	/**
 	 * String 轉換 Date
+	 * 
+	 * @param pattern 日期格式
+	 * @param date    日期資料字串
+	 * @return Date 日期資料
 	 */
 	public static Date parse(String pattern, String date) {
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
@@ -62,6 +86,10 @@ public class DateTransformUtil {
 
 	/**
 	 * Date 轉換 String
+	 * 
+	 * @param pattern 日期格式
+	 * @param date    日期資料
+	 * @return 日期資料字串
 	 */
 	public static String format(String pattern, Date date) {
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
@@ -74,6 +102,9 @@ public class DateTransformUtil {
 
 	/**
 	 * 將 LocalDateTime 轉換為 Date
+	 * 
+	 * @param date 日期資料
+	 * @return Date 日期資料
 	 */
 	private static Date transformLocalDateTimeToDate(LocalDateTime date) {
 		if (Objects.isNull(date)) {
@@ -84,14 +115,21 @@ public class DateTransformUtil {
 
 	/**
 	 * 轉換字串為 LocalDateTime
+	 * 
+	 * @param pattern 日期格式
+	 * @param date    日期資料字串
+	 * @return LocalDateTime 日期資料
 	 */
 	public static LocalDateTime transformStringToLocalDateTime(String pattern, String date) {
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
-		return LocalDateTime.parse(pattern, formatter);
+		return LocalDateTime.parse(date, formatter);
 	}
 
 	/**
 	 * 將 Date 轉換為 LocalDateTime
+	 * 
+	 * @param date    Date 日期資料
+	 * @return LocalDateTime 日期資料
 	 */
 	private static LocalDateTime transformDateToLocalDateTime(Date date) {
 		if (Objects.isNull(date)) {
